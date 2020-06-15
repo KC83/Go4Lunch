fa2066ce7aa1830e07d55c8ac2e0e7b1f23ae19a
KC83 <kellyc83780@gmail.com>
2020-06-15 17:17:22 +0200

    App style Facebook, Google and Twitter auth

---
+++ app/src/main/java/com/kcapp/go4lunch/AuthActivity.java
@@ -0,0 +1,133 @@
+package com.kcapp.go4lunch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class AuthActivity extends AppCompatActivity {
+    private ConstraintLayout mConstraintLayout;
+    private static final int RC_SIGN_IN = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        initButtons();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    private void initButtons() {
+        mConstraintLayout = findViewById(R.id.auth_activity_constraint_layout);
+        Button facebookButton = findViewById(R.id.auth_activity_button_login_facebook);
+        Button googleButton = findViewById(R.id.auth_activity_button_login_google);
+        Button twitterButton = findViewById(R.id.auth_activity_button_login_twitter);
+
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authWithFacebook();
+            }
+        });
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authWithGoogle();
+            }
+        });
+        twitterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authWithTwitter();
+            }
+        });
+    }
+    private void authWithFacebook() {
+        Log.d("TAG", "AuthActivity::authWithFacebook");
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Collections.singletonList(new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+    private void authWithGoogle() {
+        Log.d("TAG", "AuthActivity::authWithGoogle");
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Collections.singletonList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+    private void authWithTwitter() {
+        Log.d("TAG", "AuthActivity::authWithTwitter");
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Collections.singletonList(new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        Log.d("TAG", "AuthActivity::handleResponseAfterSignIn");
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                createUserInFirestore();
+                Snackbar.make(this.mConstraintLayout, getString(R.string.connection_succeed), Snackbar.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            } else { // ERRORS
+                if (response == null) {
+                    Snackbar.make(this.mConstraintLayout, getString(R.string.error_authentication_canceled), Snackbar.LENGTH_SHORT).show();
+                } else if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Snackbar.make(this.mConstraintLayout, getString(R.string.error_no_internet), Snackbar.LENGTH_SHORT).show();
+                } else if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Snackbar.make(this.mConstraintLayout, getString(R.string.error_unknown_error), Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+    private void createUserInFirestore() {
+        Log.d("TAG", "AuthActivity::createUserInFirestore");
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser != null) {
+            String urlPicture = (currentUser.getPhotoUrl() != null) ? currentUser.getPhotoUrl().toString() : null;
+            String username = currentUser.getDisplayName();
+            String uid = currentUser.getUid();
+
+            Snackbar.make(this.mConstraintLayout, "USER : username - "+username+" / uid - "+uid, Snackbar.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/com/kcapp/go4lunch/MainActivity.java
+++ app/src/main/java/com/kcapp/go4lunch/MainActivity.java
@@ -2,7 +2,11 @@ package com.kcapp.go4lunch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) {
+            Log.d("TAG", "MainActivity::currentUser is null");
+            startAuthActivity();
+        } else {
+            Log.d("TAG", "MainActivity::currentUser is not null");
+        }
+    }
+
+    private void startAuthActivity() {
+        Intent intent = new Intent(this, AuthActivity.class);
+        startActivity(intent);
     }
 }
